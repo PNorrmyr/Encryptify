46d57b8418eddf2edad2305878b9e195ea15e91e
PNorrmyr <philip.norrmyr@gmail.com>
2024-11-04 14:50:50 +0100

    Crypter class created

---
+++ backend/src/main/java/com/example/Crypter.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+
+public class Crypter {
+   private final SecretKey secretKey = this.generateKey();
+
+   public Crypter() throws NoSuchAlgorithmException {
+   }
+
+   private SecretKey generateKey() throws NoSuchAlgorithmException {
+      KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+      keyGen.init(128);
+      return keyGen.generateKey();
+   }
+
+   public String encrypt(String input) throws Exception {
+      Cipher cipher = Cipher.getInstance("AES");
+      cipher.init(1, this.secretKey);
+      byte[] encryptedBytes = cipher.doFinal(input.getBytes());
+      return Base64.getEncoder().encodeToString(encryptedBytes);
+   }
+
+   public String decrypt(String encryptedText) throws Exception {
+      Cipher cipher = Cipher.getInstance("AES");
+      cipher.init(2, this.secretKey);
+      byte[] decryptedBytes = cipher.doFinal(Base64.getDecoder().decode(encryptedText));
+      return new String(decryptedBytes);
+   }
+}
